a8816c2e358c9a00402cb1b685c9742f40fe2a42
wuzhiyi <wuzhiyi51@gmail.com>
2016-03-09 15:21:31 +0800

    user login

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.openid import OpenID
+from flask.ext.login import LoginManager
+from config import basedir
+import os
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.config.from_object('config')
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view='login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,15 +1,19 @@
-from app import app
-from flask import render_template, flash, redirect
+from app import app, lm, db, oid
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from .forms import LoginForm
+from .models import User
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = { 'nickname':'Miguel' }
+    user = g.user
     posts = [
            {
            'author': {'nickname':'John'},
-           'body': 'Beautiful day in Potland'
+           'body': 'Beautiful day in Portland'
            },
            {
             'author': {'nickname':'Susan'},
@@ -19,10 +23,46 @@ def index():
     return render_template('index_extends.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html', title='Sigin', form=form, providers=app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == '':
+        flash('Invalid login, Please try again.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=resp.nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))    
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@lm.user_loader
+def loader_user(id):
+    return User.query.get(int(id))
     
