3e76ed7e1a0c1354164621f26bcb36c10ea0b0bb
wuzhiyi <wuzhiyi51@gmail.com>
2016-03-16 16:03:25 +0800

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask.ext.openid import OpenID
 from flask.ext.login import LoginManager
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 import os
+from flask.ext.mail import Mail
+from momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -34,6 +36,9 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.info('microblog startup')
 
-from app import views, models
-from flask.ext.mail import Mail
+
 mail = Mail(app)
+
+#app.jinja_env().globals['momentjs']=momentjs
+
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from .forms import LoginForm, EditForm, PostForm, SearchForm
 from .models import User, Post
 from datetime import datetime
 from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS
+from emails import follower_notification
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -128,6 +129,7 @@ def follow(nickname):
     db.session.add(u)
     db.session.commit()
     flash('You are now following ' + nickname + '.!')
+    follower_notification(user, g.user)
     return redirect(url_for('user'), nickname=nickname)
 
 @app.route('/unfollow/<nickname>')
